8e60612f2e89f5b914de545045e5605826a3b7a0
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2023-02-15 23:42:18 +0500

    Added two main project files
    Files contain main.dart and pubspec.yaml which contains code for day2 of bootcamp.

---
+++ main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+          backgroundColor: Colors.blueGrey,
+          appBar: AppBar(
+            title: const Text("I am  Rich"),
+            backgroundColor: Colors.blueGrey[900],
+          ),
+          body: const Center(
+            child: Image(image: AssetImage('images/diamond.png')),
+          )),
+    ),
+  );
+}
+
+
+//50 lightest form and 900 darkest color
+//use ctrl+. for wrapping with widgets
+//use ctrl+space for suggestions
+//All assests will be first declared in pubspec.yaml
